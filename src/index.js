2de2223b67ea523742f39a2246a6838a2a803203
Nikitatopodin <Nikitatop1@gmail.com>
2023-01-19 16:59:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let regexpElements = '';
+  if (!isNaN(bracketsConfig[0][0] / 1)) {
+    bracketsConfig.forEach((elem) => {
+      regexpElements += elem[0]
+      regexpElements += elem[1]
+      regexpElements += '|';
+    })
+  } else {
+    bracketsConfig.forEach((elem) => {
+      if (elem != ',') {
+        regexpElements += '\\'
+        regexpElements += elem[0]
+        regexpElements += '\\'
+        regexpElements += elem[1]
+        regexpElements += '|';
+      }
+    })
+  }
+
+  let regexp = new RegExp(regexpElements.slice(0, regexpElements.length - 1))
+  if (regexp.test(str)) {
+    return check(str.replace(regexp, ''), bracketsConfig)
+  } else {
+    return '' === str
+  }
 }
+
